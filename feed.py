15f7f4be20b090d120b1f0f61f65e3d61fe9470b
Shrish Dhakal <124285180+Shrish242@users.noreply.github.com>
2024-02-20 14:40:40 +0000

    done

--- feed.py
+++ feed.py
@@ -18,16 +18,16 @@ with open('feed.yaml', 'r') as file:
     xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['author']
     xml_tree.SubElement(channel_element, 'itunes:image', {'href': link_prefix}).text = link_prefix
 
-    for item in yaml_data['item']:  # Corrected key for accessing items
+    for item in yaml_data['items']:  # Corrected the key for items
         item_element = xml_tree.SubElement(channel_element, 'item')
         xml_tree.SubElement(item_element, 'title').text = item['title']
-        xml_tree.SubElement(item_element, 'itunes:author').text = yaml_data['author']
-        xml_tree.SubElement(item_element, 'description').text = item['description']
+        xml_tree.SubElement(item_element, 'itunes:author').text = yaml_data['author']  # Should be within item_element
+        xml_tree.SubElement(item_element, 'description').text = item['description']  # Corrected key 'description'
 
-        enclosure = xml_tree.SubElement(item_element, 'enclosure', {
-            'url': link_prefix + item['file'],
+        enclosure = xml_tree.SubElement(item_element, 'enclosure', {  # Corrected indentation
+            'url': link_prefix + item['file'],  # Corrected key 'file'
             'type': 'audio/mpeg',
-            'length': str(item['length'])
+            'length': str(item['length'])  # Converted length to string
         })
 
     output_tree = xml_tree.ElementTree(rss_element)
